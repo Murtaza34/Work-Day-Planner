7ba50a745a44cbbc50ef0cda79eefb093709da6e
unknown <you@example.com>
2023-01-31 12:53:46 +0000

    Store user input to LS, updated

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,10 @@
+var timeOfDay = $("#time-of-day");
+var saveBtn = $(".save-btn");
+var clearBtn = $(".clear-btn");
+// var userInputTasks = $(".user-input");
+
 // --------------- Display Date --------------- //
-var weekDay = moment().format("DD MMMM YYYY");
+const weekDay = moment().format("DD MMMM YYYY");
 $("#date").text(weekDay);
 
 // ----------- Display current time ----------- //
@@ -11,3 +16,37 @@ function displayTime() {
 setInterval(displayTime, 1000);
 displayTime();
 
+// ------------- User input is stored when save is clicked ------------- //
+$(document).ready(function () {
+  saveBtn.on("click", function () {
+    var userInputTasks = $(".user-input")
+      .map(function () {
+        return $(this).val();
+      })
+      .get();
+
+    localStorage.setItem("usersTask", JSON.stringify(userInputTasks));
+  });
+  var userInputTasks = $(".user-input");
+  var storedTasks = localStorage.getItem("usersTask");
+  if (storedTasks) {
+    storedTasks = JSON.parse(storedTasks);
+    userInputTasks.each(function (index) {
+      $(this).val(storedTasks[index]);
+    });
+  }
+});
+
+// ----------- Get users task input from local storage ----------- //
+// $(document).ready(function () {
+//   var storedTask = localStorage.getItem("usersTask");
+//   if (storedTask) {
+//     $("#user-input").val(storedTask);
+//   }
+// });
+
+// ------------ Clear local storage and also input field ----------- //
+clearBtn.on("click", function () {
+  localStorage.removeItem("usersTask");
+  $("#user-input").val("");
+});
